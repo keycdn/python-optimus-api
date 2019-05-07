f258bb2fce60e8fab2eaa979d6e2116587bfff8e
svenba <sven@proinity.com>
2019-05-07 15:51:08 +0200

    release 0.4.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 setup(
   name = 'optimus-api',
   packages = ['optimus'],
-  version = '0.4.1',
+  version = '0.4.2',
   description = 'A Python API client for the Optimus image optimization service',
   long_description = open('README.md').read(),
   long_description_content_type = 'text/markdown',
@@ -19,5 +19,8 @@ setup(
   classifiers=[
     'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3'
+  ],
+  install_requires=[
+    'requests>=2.11.1',
   ],
 )
